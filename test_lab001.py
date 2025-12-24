be0226e052763e9f2c467e1ce3ccbd94c7667e78
vivek <vivekchandra093@gmail.com>
2025-12-24 21:59:03 +0530

    update: 24-12-2025

--- test_lab001.py
+++ test_lab001.py
@@ -1,6 +1,4 @@
-import time
-
-from playwright.sync_api import Page #calling Page class
+from playwright.sync_api import Page, expect, Playwright  # calling Page class
 
 def test_lab001(playwright): #initiate browser method 2
     browser = playwright.chromium.launch(headless=False)
@@ -19,4 +17,19 @@ def test_lab003(page: Page):
     # page.get_by_role("checkbox").click()
     page.locator("#terms").check()
     page.get_by_role("button").click()
-    time.sleep(5)
+    expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
+
+def test_firefox_browser(playwright: Playwright):
+    browser_fire = playwright.firefox.launch(headless=False)
+    context = browser_fire.new_context()
+    page = context.new_page()
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("learning")
+    page.get_by_role("combobox").select_option("teach")
+    # page.get_by_role("checkbox").click()
+    page.locator("#terms").check()
+    page.get_by_role("button").click()
+    expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
+
+

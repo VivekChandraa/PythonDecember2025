d35441c30409468b15261e8216fd5ee4fc9a9f5b
vivek <vivekchandra093@gmail.com>
2025-12-28 21:52:06 +0530

    update: 28-12-2025

--- test_placeholder.py
+++ test_placeholder.py
@@ -10,4 +10,8 @@ def test_placeholder(page:Page):
     page.get_by_role("button", name="Show").click()
     page.get_by_placeholder("Hide/Show Example").is_visible()
     page.get_by_placeholder("Hide/Show Example").fill("Vivek Chandra")
-    time.sleep(6)
+
+    #alert test
+    page.on("dialog", lambda dialog: dialog.accept())
+    page.get_by_role("button", name="Confirm").click()
+    time.sleep(2)
